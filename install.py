13f607b08caa06f7a78949deec9b836508d5ea6b
Lucas Marchesin <83802271+L0ghunt@users.noreply.github.com>
2022-12-20 19:34:41 -0300

    Update install.py
    up

--- install.py
+++ install.py
@@ -2,21 +2,30 @@ from os import *
 from os import geteuid
 
 #validate root
-def validate_root():
-    if not geteuid() == 0:
-        print("-] Please run this script as root... [-]")
-        exit()
-    else:
-        pass
-#dowload go
-def dowload_go():
-    print('[+] Golang Download Start... [+]')
-    command_download_go = 'wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz'
-    command_install = 'rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz'
-    command_move_go_1 = 'cd /usr/local/go/bin'
-    command_move_go_2 = 'mv * /usr/bin'
-    command_go_version = 'go version'
-    print('[+] Golang Download finished... [+]')
+class Install(object):
+    def __init__(self, start):
+        self.start = start
+        
+    def validate_root(self):
+        if not geteuid() == 0:
+            print("-] Please run this script as root... [-]")
+            exit()
+        else:
+            pass
+
+    def download_go(self):
+        print('[+] Golang Download Start... [+]')
+        command_download_go = 'wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz'
+        popen(command_download_go).read()
+        command_install = 'rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz'
+        popen(command_install).read()
+        command_move_go_1 = 'cd /usr/local/go/bin'
+        popen(command_move_go_1).read()
+        command_move_go_2 = 'mv * /usr/bin'
+        popen(command_move_go_2)
+        command_go_version = 'go version'
+        popen(command_go_version).read()
+        print('[+] Golang Download finished... [+]')
 
 #dowload go tools via go install
 # def dowload_amass():
@@ -53,6 +62,7 @@ def dowload_go():
 #     print('[+] Dalfox Download Start... [+]')
 #     command_download = 'go install github.com/hahwul/dalfox/v2@latest'
 #     print('[+] Dalfox Download Finished... [+]')
-
-
-print('[-]Finished[-]')
+user_start_input = str(input("Digite Start para começar"))
+install_to_install = Install(user_start_input)
+install_to_install.validate_root()
+install_to_install.download_go()
