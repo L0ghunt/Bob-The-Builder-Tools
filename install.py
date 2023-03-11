d17fa4811c31ed897ff25db4abf4418be0f89542
Lucas Marchesin <83802271+L0ghunt@users.noreply.github.com>
2023-03-11 15:49:18 -0300

    Update install.py
    att

--- install.py
+++ install.py
@@ -61,35 +61,35 @@ def download_amass():
 
 
 
-# def dowload_anew():
-#     print('[+] Anew Download Start... [+]')
-#     command_download = 'go install -v github.com/tomnomnom/anew@latest'
-#     print('[+] Anew Download Finished... [+]')
+def dowload_anew():
+    print('[+] Anew Download Start... [+]')
+    command_download = 'go install -v github.com/tomnomnom/anew@latest'
+    print('[+] Anew Download Finished... [+]')
 
-# def dowload_airixss():
-#     print('[+] Airixss Download Start... [+]')
-#     command_download = 'go install github.com/ferreiraklet/airixss@latest'
-#     print('[+] Airixss Download Finished... [+]')
+def dowload_airixss():
+    print('[+] Airixss Download Start... [+]')
+    command_download = 'go install github.com/ferreiraklet/airixss@latest'
+    print('[+] Airixss Download Finished... [+]')
 
-# def dowload_cfchek():
-#     print('[+] Cfchek Download Start... [+]')
-#     command_download = '$ go install github.com/dwisiswant0/cf-check@latest'
-#     print('[+] Cfchek Download Finished... [+]')
+def dowload_cfchek():
+    print('[+] Cfchek Download Start... [+]')
+    command_download = '$ go install github.com/dwisiswant0/cf-check@latest'
+    print('[+] Cfchek Download Finished... [+]')
 
-# def dowload_chaos():
-#     print('[+] Chaos Download start... [+]')
-#     command_download = 'go install -v github.com/projectdiscovery/chaos-client/cmd/chaos@latest'
-#     print('[+] Chaos Download Finished... [+]')
+def dowload_chaos():
+    print('[+] Chaos Download start... [+]')
+    command_download = 'go install -v github.com/projectdiscovery/chaos-client/cmd/chaos@latest'
+    print('[+] Chaos Download Finished... [+]')
 
-# def dowload_cariddi():
-#     print('[+] Cariddi Download Start... [+]')
-#     command_download = 'go install -v github.com/edoardottt/cariddi/cmd/cariddi@latest'
-#     print('[+] Cariddi Download Finished... [+]')
+#def dowload_katana():
+#    print('[+] katana Download Start... [+]')
+#    command_download = ''
+#    print('[+] Katana Download Finished... [+]')
 
-# def dowload_dalfox():
-#     print('[+] Dalfox Download Start... [+]')
-#     command_download = 'go install github.com/hahwul/dalfox/v2@latest'
-#     print('[+] Dalfox Download Finished... [+]')
+def dowload_dalfox():
+    print('[+] Dalfox Download Start... [+]')
+    command_download = 'go install github.com/hahwul/dalfox/v2@latest'
+    print('[+] Dalfox Download Finished... [+]')
 
 print("""  ____                            _            __  __""")
 print(""" | __ )    ___    _   _   _ __   | |_   _   _  \ \/ /""")
@@ -109,6 +109,11 @@ install_to_install.Mover_go()
 install_to_install.Mover_gofmt()
 install_to_install.Vericar_go()
 install_to_install.download_amass()
-
+install_to_install.download_anew()
+install_to_install.download_airixss()
+install_to_install.download_cfchek()
+install_to_install.download_chaos()
+#install_to_install.download_katana()
+install_to_install.download_dalfox()
 
 
