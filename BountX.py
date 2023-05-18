c350bfaf11c54035c42a8fa96f1afc256462c2e9
Lucas Marchesin <83802271+L0ghunt@users.noreply.github.com>
2023-05-17 21:11:25 -0300

    Update BountX.py
    att

--- BountX.py
+++ BountX.py
@@ -11,32 +11,29 @@ class B0untyX(object):
         else:
             pass
 
-    def domain_to_enumerate(self):
+    def domain_to_enumerate1(self):
         domain_to_enumerate_command = 'echo '+self.domain+' | subfinder -silent | dnsx | anew '+self.domain+'_domains'
         print("[*] domain enumarete execute process starting... [*]")
         os.popen(domain_to_enumerate_command).read()
         print('[+] domain to enumerate finished... [+]')
 
-    # def Take_ouver(self):
-    #     desenvolvimento
+    # def valit_domains(self):
+    #     valit_domains_command = 'cat '+self.domain+'_domains | httpx -silent -mc 200 | anew '+self.domain+'_200_domains'
+    #     print("[*] Valit domains execute process starting... [*]")
+    #     os.popen(valit_domains_command).read()
+    #     print("[*] Valit domains execute finished... [*]")
 
-    def valit_domains(self):
-        valit_domains_command = 'cat '+self.domain+'_domains | httpx -silent -mc 200 | anew '+self.domain+'_200_domains'
-        print("[*] Valit domains execute process starting... [*]")
-        os.popen(valit_domains_command).read()
-        print("[*] Valit domains execute finished... [*]")
+    # def urls_to_param(self):
+    #     urls_to_command = 'cat '+self.domain+'_200_domains | gauplus | uro | anew '+self.domain+'_urls'
+    #     print("[*] urls to crawlers metodh 2 execute process starting... [*]")
+    #     os.popen(urls_to_command).read()
+    #     print("[+] urls to crawlers finished... [+]")
 
-    def urls_to_param(self):
-        urls_to_command = 'cat '+self.domain+'_200_domains | gauplus | uro | anew '+self.domain+'_urls'
-        print("[*] urls to crawlers metodh 2 execute process starting... [*]")
-        os.popen(urls_to_command).read()
-        print("[+] urls to crawlers finished... [+]")
-
-    def nuclei_scan(self):
-        nuclei_scan_command = 'nuclei -l '+self.domain+'_urls -t /root/nuclei-templates/* -es info | notify'
-        print(" [*] Nuclei scan execute starting... [*]")
-        os.popen(nuclei_scan_command).read()
-        print(" [+] Nuclei scan execute finished [+]")
+    # def nuclei_scan(self):
+    #     nuclei_scan_command = 'nuclei -l '+self.domain+'_urls -t /root/nuclei-templates/* -es info | notify'
+    #     print(" [*] Nuclei scan execute starting... [*]")
+    #     os.popen(nuclei_scan_command).read()
+    #     print(" [+] Nuclei scan execute finished [+]")
 
     
 
@@ -52,6 +49,6 @@ user_domain_input = str(input("[+] Enter domain to scan >>  [ EX: domain.com.br 
 domain_to_scan = B0untyX(user_domain_input)
 domain_to_scan.validate_run_as_root()
 domain_to_scan.domain_to_enumerate()
-domain_to_scan.valit_domains()
-domain_to_scan.ulrs_to_param()
-domain_to_scan.nuclei_scan()
+# domain_to_scan.valit_domains()
+# domain_to_scan.ulrs_to_param()
+# domain_to_scan.nuclei_scan()
