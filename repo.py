8881ad589c0c05194ad8cdddec2ec89161e63052
Lucas Marchesin <83802271+L0ghunt@users.noreply.github.com>
2023-05-18 14:28:06 -0300

    Create repo.py
    aat

---
+++ repo.py
@@ -0,0 +1,25 @@
+# Repositories
+repositories = [
+    "github.com/projectdiscovery/subfinder/v2/cmd/subfinder",
+    "github.com/Edu4rdSHL/findomain",
+    "github.com/michenriksen/aquatone",
+    "github.com/projectdiscovery/dnsx/cmd/dnsx",
+    "github.com/tomnomnom/tlsenum/cmd/tlsenum",
+    "github.com/projectdiscovery/puredns/cmd/puredns",
+    "github.com/maurosoria/dirsearch",
+    "github.com/tomnomnom/anew",
+    "github.com/tomnomnom/unfurl",
+    "github.com/JohnWoodman/katana",
+    "github.com/projectdiscovery/shuffledns/cmd/shuffledns"
+    "github.com/projectdiscovery/naabu/v2/cmd/naabu"
+    "github.com/tomnomnom/assetfinder"
+    "github.com/ferreiraklet/airixss"
+    "github.com/dwisiswant0/cf-check"
+    "github.com/projectdiscovery/chaos-client/cmd/chaos"
+    "github.com/edoardottt/cariddi/cmd/cariddi"
+    "github.com/hahwul/dalfox/v2"
+    "github.com/tomnomnom/hacks/filter-resolved"
+    "github.com/ffuf/ffuf/v2"
+    "github.com/takshal/freq"
+
+]
