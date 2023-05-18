0e5e8a362756b763047545e0dc871fd782da7988
Lucas Marchesin <83802271+L0ghunt@users.noreply.github.com>
2023-05-18 14:27:59 -0300

    Update install.py
    att

--- install.py
+++ install.py
@@ -1,28 +1,12 @@
 import os
+from repo import repositories
 
-# Repositories
-repositories = [
-    "github.com/projectdiscovery/subfinder/v2/cmd/subfinder",
-    "github.com/Edu4rdSHL/findomain",
-    "github.com/michenriksen/aquatone",
-    "github.com/projectdiscovery/dnsx/cmd/dnsx",
-    "github.com/tomnomnom/tlsenum/cmd/tlsenum",
-    "github.com/projectdiscovery/puredns/cmd/puredns",
-    "github.com/maurosoria/dirsearch",
-    "github.com/tomnomnom/anew",
-    "github.com/tomnomnom/unfurl",
-    "github.com/JohnWoodman/katana",
-    "github.com/projectdiscovery/shuffledns/cmd/shuffledns"
-]
 
 # Install Go
 os.system("wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz")
 os.system("rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz")
 os.system("mv /usr/local/go/bin/* /usr/bin")
 os.system("go version")
-
-# Add Go path to .bashrc
-os.system("echo 'export PATH=$PATH:/usr/local/go/bin' >> /root/.bashrc")
 
 # Install tools
 for repo in repositories:
