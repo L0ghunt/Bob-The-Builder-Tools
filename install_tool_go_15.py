b76d2d9bc5dbaa63791dedbb0af81ad2d01e89d0
Lucas Marchesin <83802271+L0ghunt@users.noreply.github.com>
2023-05-26 22:39:14 -0300

    Update install_tool_go_15.py

--- install_tool_go_15.py
+++ install_tool_go_15.py
@@ -5,11 +5,9 @@ from repo import repositories
 # Install Go
 os.system("wget https://go.dev/dl/go1.15.1.linux-amd64.tar.gz")
 os.system("rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz")
-os.system("cd /usr/bin")
-os.system("rm -fr go gofmt")
-os.system("cd /usr/local/go/bin")
-os.system("mv * /usr/bin")
-os.system("cd /home")
+os.system("rm -fr /usr/bin/go")
+os.system("rm -fr /usr/bin/gofmt")
+os.system("mv /usr/local/go/bin/* /usr/bin")
 os.system("go version")
 
 # Install tools
