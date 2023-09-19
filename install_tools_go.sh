9499c678e7d743b5767b8dafa5dd1c3ac983e4b4
Lucas Marchesin <83802271+L0ghunt@users.noreply.github.com>
2023-09-19 12:43:47 -0300

    Update install_tools_go.sh

--- install_tools_go.sh
+++ install_tools_go.sh
@@ -1,6 +1,7 @@
 echo "Start install golang 1.20.4"
+echo "Download golang start"
 wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
-echo "Finished install golang"
+echo "Download golang Fineshed"
 echo "Start descompact"
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
 echo "Fineshed descompact"
@@ -10,3 +11,6 @@ echo "Binarios movidos"
 echo "Verificando instalação"
 go version
 echo "Verificação concluida"
+#tools
+
+echo "Finished install golang"
