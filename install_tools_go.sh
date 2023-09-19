5391cc939537a2fdd63d8ebeceb7752382139e19
Lucas Marchesin <83802271+L0ghunt@users.noreply.github.com>
2023-09-19 14:25:37 -0300

    remove

--- install_tools_go.sh
+++ install_tools_go.sh
@@ -11,6 +11,28 @@ echo "Binarios movidos"
 echo "Verificando instalação"
 go version
 echo "Verificação concluida"
-#tools
-
 echo "Finished install golang"
+#tools
+echo "Start intall tools"
+echo "Install Nuclei"
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+echo "Finished"
+echo "Install Subfinder"
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+echo "Finished"
+echo "Install Httpx"
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+echo "Finished"
+echo "Install Naabu"
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+echo "Finished"
+echo "Install Interactsh"
+go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest
+echo "Finished"
+echo "Install Notify"
+go install -v github.com/projectdiscovery/notify/cmd/notify@latest
+echo "Finished"
+echo "Install Tlsx"
+go install github.com/projectdiscovery/tlsx/cmd/tlsx@latest
+echo "Fineshed"
+echo 
